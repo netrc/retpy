fa5c3c4fed6c8bb1c568955f6a19e95b063d24fe
netrc <ric@netrc.com>
2017-05-29 14:24:40 -0400

    rewrote to move summary calculations into own class

--- lib/Ret.py
+++ lib/Ret.py
@@ -11,6 +11,11 @@ class Portfolio:            # or should just be a global
         self.S = Scenario(f)
         self.inflation = 1.03
 
+        # Make our Summary 'columns'
+        RSummary("Inc")
+        RSummary("Exp")
+        RSummary("Inv")
+
     def ritemsAppend(self,r):
         self.ritems.append(r)
 
@@ -18,38 +23,35 @@ class Portfolio:            # or should just be a global
         return self.cash;
 
     def netWorth(self):
-        return( self.cash + Ritem_income.value() + Ritem_inv.value() - Ritem_expense.value() )
+        return  self.cash + RSummary.value("Inc") + RSummary.value("Inv") - RSummary.value("Exp")
 
     def summaryString(self):
-        return "c= ${} ${} e= ${} i=${:.0f} ==> ${:.0f}".format(self.cash, Ritem_income.value(), Ritem_expense.value(), Ritem_inv.value(), self.netWorth())
+        return "c= ${} ${} e= ${} i=${:.0f} ==> ${:.0f}".format(self.cash, RSummary.value("Inc"), RSummary.value("Exp"), RSummary.value("Inv"), self.netWorth())
         
 
     def run(self,startYear,endYear):
         logging.debug("INIT: {}".format(self.summaryString()))
-        for y in range(startYear, endYear):
+        for y in range(startYear, endYear+1):
             self.S.addYear(y)
             logging.debug("Starting: {}".format(y))
-            Ritem_inv.invSumReset()
-            Ritem_income.incSumReset()
-            Ritem_expense.expSumReset()
-            for r in [r for r in self.ritems if r.status]:
-                #logging.debug("...doing item: {}".format(r.name))
-                # each "event" is a value ?? or a transfer and a value??
-                r.reset()
-                r.currentYear = y   # used for various Ritems, e.g. expenseInflation
-                for e in r.events:
-                    if (e.year == y):
-                        #logging.debug ("... run event: {}".format(e.name))
-                        e.func(r)
-                # after all the events are done
-                self.S.addColVal(r.name, y,r.value)
-                r.sumToSummary()
+            for sname in RSummary._sumItems:
+                for r in RSummary._sumItems[sname].ritems:
+                    #logging.debug("...doing item: {}".format(r.name))
+                    # each "event" is a value ?? or a transfer and a value??
+                    r.reset()
+                    r.currentYear = y   # used for various Ritems, e.g. expenseInflation
+                    for e in r.events:
+                        if (e.year == y):
+                            #logging.debug ("... run event: {}".format(e.name))
+                            e.func(r)
+                    # after all the events are done
+                    self.S.addColVal(r.name, y,r.value)
+                    #r.sumToSummary()
+                self.S.addColVal(sname,y,RSummary.value(sname))
+
             logging.debug("{}: {}".format(y, self.summaryString()))
             self.S.addColVal('NetW',y,self.netWorth())
-            self.S.addColVal('Inc',y,Ritem_income.value())
-            self.S.addColVal('Exp',y,Ritem_expense.value())
-            self.S.addColVal('Inv',y,Ritem_inv.value())
-            self.cash += Ritem_income.value() - Ritem_expense.value();
+            self.cash += RSummary.value("Inc") - RSummary.value("Exp");
     def runTilEnd(self,startYear):
         self.run(startYear,self.family.lastYear())
 
@@ -90,58 +92,52 @@ class Ritem():             # think of this as a column in the sp-sheet
         r.addValue(n,v)
         logging.debug("transfer {} {} + ${} => ${}".format(self.name,n,v,self.value))
 
+# Another type of column; this is mostly a collection of similar typed columns, _income, _expense, etc
+class RSummary():
+    _sumItems = {}
+    def __init__(self,name):      # "Inc", "Exp", "Inv"...
+        self.name = name
+        self.ritems = []      # list of Ritems per Summary
+        RSummary._sumItems[name] = self    # a class var (no self!)   # list of all Summary items
+                                          # no need to check for existence; should (!) be unique
+    def addRitem(name,r):    # a class method (no self!)
+        RSummary._sumItems[name].ritems.append(r)
+    def value(name): # a class method (no self!)
+        return sum(r.value for r in RSummary._sumItems[name].ritems)
 
 # This is a column type - used to manage income, adds up to Income pseudo-col
 class Ritem_income(Ritem):
-    _incSumValue = 0 # class variable
     def __init__(self,p,name):
-        super().__init__(p,name)
+        super().__init__(p,name)          # there's a main ritem list in the superclass
+        RSummary.addRitem("Inc",self)     # and then a collection list in this named summary column
     def addIncome(self,n,v):
         self.value += v
         logging.debug("income {} {} + ${} => ${}".format(self.name,n,v,self.value))
     def addValue(self,n,v):
     		self.addIncome(n,v)
-    def sumToSummary(self):
-        #print("inv sum to summary")
-        Ritem_income._incSumValue += self.value
-    # class (summary column) methods   # no self
-    def incSumReset():
-        #print("inv sum reset")
-        Ritem_income._incSumValue = 0
-    def value():
-        return Ritem_income._incSumValue
 
 # This is a column type - used to manage expenses, adds up to expense pseudo-col
 class Ritem_expense(Ritem):
-    _expSumValue = 0 # class variable
     def __init__(self,p,name):
         super().__init__(p,name)
+        RSummary.addRitem("Exp",self)     # and then a collection list in this named summary column
     def takeExpense(self,n,v):
         #print("expense {} {} before ${}".format(self.name,n,self.value))
         self.value += v
         logging.debug("expense {} {} + ${} => ${}".format(self.name,n,v,self.value))
     def takeExpenseInflation(self,n,v,baseYear):
         logging.debug("expenseInflation {} {} before ${}".format(self.name,n,self.value))
-        print("ei: {} {} {} {} {} {}".format(self.name,n,self.value,self.currentYear,baseYear,self.portfolio.inflation))
+        #print("ei: {} {} {} {} {} {}".format(self.name,n,self.value,self.currentYear,baseYear,self.portfolio.inflation))
         self.value += v * self.portfolio.inflation**(self.currentYear-baseYear)
       # add in years
         logging.debug("expense {} {} + ${} => ${}".format(self.name,n,v,self.value))
-    def sumToSummary(self):
-        Ritem_expense._expSumValue += self.value
-        #print("exp sum to sum = {}".format(Ritem_expense._expSumValue))
-    # class (summary column) methods   # no self
-    def expSumReset():
-        Ritem_expense._expSumValue = 0
-        #print("exp sum reset = {}".format(Ritem_expense._expSumValue))
-    def value():
-        return Ritem_expense._expSumValue
 
 
 # This is a column type - used to manage investments, adds up to Inv pseudo-col
 class Ritem_inv(Ritem):
-    _invSumValue = 0 # class variable
     def __init__(self,p,name):
         super().__init__(p,name)
+        RSummary.addRitem("Inv",self)     # and then a collection list in this named summary column
     def reset(self):
         pass            # for investments, don't reset every year; n.b. overrides base class
     def setValue(self,n,v):
@@ -153,15 +149,6 @@ class Ritem_inv(Ritem):
     def addAPR(self,n,rate):
         self.value *= rate
         logging.debug("inv {} {} apr ${} => ${}".format(self.name,n,rate,self.value))
-    def sumToSummary(self):
-        #print("inv sum to summary")
-        Ritem_inv._invSumValue += self.value
-    # class (summary column) methods   # no self
-    def invSumReset():
-        #print("inv sum reset")
-        Ritem_inv._invSumValue = 0
-    def value():
-        return Ritem_inv._invSumValue
 
 import unittest
 
@@ -172,14 +159,15 @@ class TC1(unittest.TestCase):
         F.addPerson(Person("John","j",1970))
         P = Portfolio(F)
         joeI = Ritem_income(P,'joeIncome')
-        joeI.addEvents(2016,2022, lambda r: (r.addIncome('comp1',2)))
-        P.run(2016,2022)    # 7 years, $14
-        self.assertEqual(P.netWorth(), 14)
+        joeI.addEvents(2016,2022, lambda r: (r.addIncome('comp1',2))) # 7 years, $14
+        joeI.addEvents(2021,2022, lambda r: (r.addIncome('comp1',4))) # 2 years, $8  // bonus
+        joeI2 = Ritem_income(P,'joeIncome2')
+        joeI2.addEvents(2017,2019, lambda r: (r.addIncome('comp2',3)))  # 3 years, $9
+        P.run(2016,2022)    
+        #self.assertEqual(P.netWorth(), 31) # $14 + $8 + $9 = $31
+        self.assertEqual(P.netWorth(), 37) # $14 + $8 + $9 = $31
+        P.S.printRawCols()
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
 
